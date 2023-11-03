3e8df27be6e0fe0bcc7fbc6d5f82c15a2d7a54a4
Mazin Hafiz <hajiki.326@hotmail.com>
2023-11-03 06:21:52 +0200

    :sparkles: Initializing

---
+++ README.md
@@ -0,0 +1,42 @@
+
+# Places
+
+the Backend side of the places application
+
+
+## Tech Stack
+
+**Server:** Node, Express
+
+
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone https://github.com/3neef/places-Backend.git
+```
+
+Go to the project directory
+
+```bash
+  cd places-Backend
+```
+
+Install dependencies
+
+```bash
+  npm install
+```
+
+Start the server
+
+```bash
+  npm run start
+```
+
+
+### Author
+
+- [@Mazin](https://www.github.com/3neef)
+
---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 4030
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/user', (req, res, next) => {
+  res.send('<h1>User: ' + req.body.username + '</h1>');
+});
+
+app.get('/', (req, res, next) => {
+  res.send(
+    '<form action="/user" method="POST"><input type="text" name="username"><button type="submit">Create User</button></form>'
+  );
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
